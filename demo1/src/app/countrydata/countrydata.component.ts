1fc50b8a9ecce0c0574bd8e25b03cfb1ad786206
fkm <shaikhmiran@gmail.com>
2019-03-27 10:16:15 +0530

    first commit

---
+++ demo1/src/app/countrydata/countrydata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-countrydata',
+  templateUrl: './countrydata.component.html',
+  styleUrls: ['./countrydata.component.css']
+})
+export class CountrydataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ demo1/src/app/employedata/employedata.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employedata',
+  templateUrl: './employedata.component.html',
+  styleUrls: ['./employedata.component.css']
+})
+export class EmployedataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
